f3328c8f6ad9e7447ad22dbacbf45a3dd205a424
Camilly Vitória <vitoriacamilly308@gmail.com>
2025-08-31 20:45:18 -0300

    add class Bootcamp and Dev

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,55 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudoInscrito = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluido = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudoInscrito() {
+        return conteudoInscrito;
+    }
+
+    public void setConteudoInscrito(Set<Conteudo> conteudoInscrito) {
+        this.conteudoInscrito = conteudoInscrito;
+    }
+
+    public Set<Conteudo> getConteudoConcluido() {
+        return conteudoConcluido;
+    }
+
+    public void setConteudoConcluido(Set<Conteudo> conteudoConcluido) {
+        this.conteudoConcluido = conteudoConcluido;
+    }
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+    public void progredir(){}
+
+    public void calcularTotalXp() {}
+
+    // Compara dois objetos Dev com base em nome, conteudos inscritos e concluídos
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudoInscrito, dev.conteudoInscrito) && Objects.equals(conteudoConcluido, dev.conteudoConcluido);
+    }
+
+    // Gera hashCode com base nos mesmos campos usados no equals()
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudoInscrito, conteudoConcluido);
+    }
+}
